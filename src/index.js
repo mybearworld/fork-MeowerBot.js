10b9587d8215ee10236a44cdd0de6e44b32aa594
M.D. Walters <808426@pdsb.net>
2022-12-01 11:44:45 -0500

    1.1.0

--- src/index.js
+++ src/index.js
@@ -1,19 +1,65 @@
 import WebSocket from "ws";
 import fetch from "node-fetch";
 
+var user;
+var pass;
+
 console.log("Connecting...");
 const ws = new WebSocket("wss://server.meower.org/");
 
+ws.on("message", () => {
+    if (messageData.cmd === "ping") {
+        if (messageData.val === "I:100 | OK") {
+            console.log("Ping is OK");
+        } else {
+            console.error("Ping is not OK");
+        }
+    } else if (messageData.val.state === 101 || messageData.val.state === 100) {
+        console.log(`${messageData.val.u} is typing...`);
+    } else if (messageData.cmd === "ulist") {
+        console.log(`Users online: ${messageData.val.split(";").join(", ")}`);
+    } else if (messageData.cmd === "statuscode") {
+        if (messageData.val.startsWith("E")) {
+            console.error(`Status: ${messageData.val}`);
+        } else if (messageData.val.startsWith("I:100")) {
+            console.log(`Status: ${messageData.val}`);
+        } else {
+            console.log(`Status: ${messageData.val}`);
+        }
+    } else if (messageData.val.cmd === "motd") {
+        console.log(`MOTD: ${messageData.val.val}`);
+    } else if (messageData.val.cmd === "vers") {
+        console.log(`Meower Server Version: ${messageData.val.val}`);
+    } else if (messageData.val.state === 1) {
+        console.log(`${messageData.val.u} joined ${messageData.val.chatid}`);
+    } else if (messageData.val.state === 0) {
+        console.log(`${messageData.val.u} left ${messageData.val.chatid}`);
+    } else if (messageData.val.mode === "auth") {
+        console.log(`Logged in as "${messageData.val.payload.username}" (${messageData.val.payload.token})`);
+    } else {
+        console.log(`New message: ${data}`);
+    }
+});
+
+setInterval(() => {
+    if (ws.readyState == 1) {
+        ws.send('{"cmd": "ping", "val": ""}');
+    }
+}, 10000);
+
 export default class Bot {
     constructor(username, password, callback) {
-        ws.on("open", async function() {
+        var user = username;
+        var pass = password;
+
+        ws.on("open", () => {
             console.log("Connected");
             ws.send(`{"cmd": "direct", "val": {"cmd": "type", "val": "js"}}`);
             ws.send(`{"cmd": "direct", "val": {"cmd": "ip", "val": "${await fetch("https://api.meower.org/ip").then(res => res.text())}"}}`);
             ws.send(`{"cmd": "direct", "val": "meower"}`);
             ws.send(`{"cmd": "direct", "val": {"cmd": "version_chk", "val": "scratch-beta-5-r7"}}`);
             ws.send(`{"cmd": "direct", "val": {"cmd": "authpswd", "val": {"username": "${username}", "pswd": "${password}"}}}`);
-            setTimeout(function() {
+            setTimeout(() => {
                 callback();
             }, 1000);
         });
@@ -23,14 +69,17 @@ export default class Bot {
         ws.send(JSON.stringify({"cmd": "direct", "val": {"cmd": "post_home", "val": content}}));
     }
 
-    handlePost(callback) {
-        ws.on("message", async function(data) {
+    onPost(callback) {
+        ws.on("message", (data) => {
             var messageData = JSON.parse(data);
             
             if (messageData.val.type === 1) {
                 try {
                     console.log(`New post: ${messageData.val.u}: ${messageData.val.p}`);
-                    if (messageData.val.u == "Discord") {
+
+                    if (messageData.val.u === user) {
+                        return;
+                    } else if (messageData.val.u == "Discord") {
                         callback(messageData.val.p.split(": ")[0], messageData.val.p.split(": ")[1]);
                     } else {
                         callback(messageData.val.u, messageData.val.p);
@@ -39,6 +88,13 @@ export default class Bot {
                     return;
                 }
             }
+        });
+    }
+
+    onClose(callback) {
+        ws.on("close", () => {
+            console.error("Disconnected");
+            callback();
         });
     }
 }
