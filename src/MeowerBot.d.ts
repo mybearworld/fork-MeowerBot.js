894708fd92e3d3b98b3515862df297a14edd3139
M.D. Walters <popularvsofficial@gmail.com>
2023-03-02 12:03:36 -0500

    yes

--- src/MeowerBot.d.ts
+++ src/MeowerBot.d.ts
@@ -1,10 +1,10 @@
 declare class Bot {
     constructor(username: string, password: string, server?: string, prefix?: string);
     post(content: string, origin?: string): void;
-    onPost(callback: (username: string, content: string, origin: string | null) => void): void;
-    onClose(callback: () => void): void;
-    onMessage(callback: (data: string) => void): void;
-    onLogin(callback: () => void): void;
+    onPost(callback: Function): void;
+    onClose(callback: Function): void;
+    onMessage(callback: Function): void;
+    onLogin(callback: Function): void;
     onCommand(command: string, callback: Function): void;
     send(message: object): void;
 }
