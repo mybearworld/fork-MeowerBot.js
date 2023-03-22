c2849e6d4dd787377adcd39b5d42bbdeefd5adb3
M.D. Walters <808426@pdsb.net>
2023-03-22 19:59:13 -0400

    Figure it out for yourself

--- tests/SimpleBot.js
+++
@@ -1,9 +0,0 @@
-import Bot from "../src/MeowerBot.js";
-
-const bot = new Bot();
-
-bot.onCommand("help", (ctx) => {
-    ctx.reply("I don't have any commands!");
-});
-
-bot.login("m", "camilla124");
