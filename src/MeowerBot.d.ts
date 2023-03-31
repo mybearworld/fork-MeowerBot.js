769cb8618b818ae34d0872e5105a20422753534c
M.D. Walters <808426@pdsb.net>
2023-03-31 07:28:17 -0400

    The return of `?`

--- src/MeowerBot.d.ts
+++ src/MeowerBot.d.ts
@@ -1,6 +1,6 @@
 declare class Bot {
     login(username: string, password: string, server?: string, prefix?: string);
-    post(content: string, origin: string | null): void;
+    post(content: string, origin?: string | null): void;
     onPost(callback: Function): void;
     onClose(callback: Function): void;
     onMessage(callback: Function): void;
