8d4b2a4ffce7b4845172a6959143edcd5059fdad
M.D. Walters <808426@pdsb.net>
2023-06-05 09:11:59 -0400

    fix: I forgor to remove :skull:

--- src/MeowerBot.d.ts
+++ src/MeowerBot.d.ts
@@ -2,11 +2,8 @@ export interface Message {
     cmd: string;
     val: any;
 }
-/* Nice going! You broke it.
-export interface Context {
-    The evolution of the word "Hi" on Meower: 2020: Hi, 2022: h, 2023: @wlbot work
-}
-*/
+
+// export interface Context {}
 
 declare class Bot {
     login(username: string, password: string, server?: string, prefix?: string);
