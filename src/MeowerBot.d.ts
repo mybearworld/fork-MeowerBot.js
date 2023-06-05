fad4196d8bccaed5fe734428c48e807254d6934d
M.D. Walters <popularvsofficial@gmail.com>
2023-06-05 09:21:51 -0400

    fix: finally fix the context interface

--- src/MeowerBot.d.ts
+++ src/MeowerBot.d.ts
@@ -3,11 +3,17 @@ export interface Message {
     val: any;
 }
 
-// export interface Context {}
+export interface Context {
+    user: string;
+    args: string[];
+    origin: string;
+    reply: (content: string) => void;
+    post: (content: string) => void;
+}
 
 declare class Bot {
     login(username: string, password: string, server?: string, prefix?: string);
-    post(content: string, origin?: string | null): void;
+    post(content: string, origin: string | null): void;
     onPost(callback: () => void): void;
     onClose(callback: () => void): void;
     onMessage(callback: () => void): void;
@@ -15,7 +21,7 @@ declare class Bot {
     onCommand(command: string, callback: (ctx: Context) => void): void;
     send(message: Message): void;
     close(): void;
-    onCommandMiddleware(callback: (ctx: any) => boolean): void;
+    onCommandMiddleware(callback: (ctx: Context) => boolean): void;
 }
 
 export = Bot;
